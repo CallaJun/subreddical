b52e2694a77a9e9f5c6480c5e0ca4576d5ec6c50
Calla <maracondo@aol.com>
2017-02-05 11:08:57 -0500

    Download data as json

--- popup.js
+++ popup.js
@@ -15,6 +15,18 @@ function refreshPopup() {
 
 // Clears data
 function clearActivity() {
+  // Download CSV
+  chrome.storage.local.get('activity', function(data) {
+      var result = JSON.stringify(data);
+
+      // Save as file
+      var url = 'data:application/json;base64,' + btoa(result);
+      chrome.downloads.download({
+          url: url,
+          filename: 'subreddical_activity.json'
+      });
+  });
+
   chrome.extension.getBackgroundPage().clearActivity();
   refreshPopup();
 }
